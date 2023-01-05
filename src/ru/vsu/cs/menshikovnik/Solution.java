d08b20199eb784d3fbb1e020faf8f14f4d5e7c37
nikmenshikov <kolya.menshikov.03@list.ru>
2023-01-06 02:11:27 +0300

    version 1.2

--- src/ru/vsu/cs/menshikovnik/Solution.java
+++ src/ru/vsu/cs/menshikovnik/Solution.java
@@ -7,10 +7,9 @@ public class Solution {
     public static String findElement(String text) {
         String[] str1 = text.split(" ");
         int i = 0;
-        char[] ch;
         List<String> end = new ArrayList<>();
         for (String element : str1) {
-            ch = element.toCharArray();
+            char[] ch = element.toCharArray();
             for (char c : ch) {
                 if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
                     String result = new String(ch);
