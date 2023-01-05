7bf22826921dfff2311888b8baea4b822d89f10f
nikmenshikov <kolya.menshikov.03@list.ru>
2023-01-06 01:14:00 +0300

    version 1.0

---
+++ src/ru/vsu/cs/menshikovnik/Solution.java
@@ -0,0 +1,26 @@
+package ru.vsu.cs.menshikovnik;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static String findElement(String text) {
+        String[] str1 = text.split(" ");
+        String result;
+        int i = 0;
+        char[] ch;
+        List<String> end = new ArrayList<>();
+        for (String element : str1) {
+            ch = element.toCharArray();
+            for (char c : ch) {
+                if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
+                    result = new String(ch);
+                    end.add(i, result);
+                    i++;
+                }
+            }
+        }
+        String delim = " ";
+        return String.join(delim, end);
+    }
+}
