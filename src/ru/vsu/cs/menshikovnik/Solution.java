062693945b2526bd3eac765cd5dabf2a5bf04669
nikmenshikov <kolya.menshikov.03@list.ru>
2023-01-08 16:19:35 +0300

    version 1.4

--- src/ru/vsu/cs/menshikovnik/Solution.java
+++ src/ru/vsu/cs/menshikovnik/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
         for (String element : str1) {
             char[] ch = element.toCharArray();
             for (char cyrillic : ch) {
-                if ((cyrillic >= 'А' && cyrillic <= 'Я') || (cyrillic >= 'а' && cyrillic <= 'я')) {
+                if ((cyrillic >= 'А' && cyrillic <= 'Я') || (cyrillic >= 'а' && cyrillic <= 'я')){
                     for (char latin : ch) {
                         if ((latin >= 'A' && latin <= 'Z') || (latin >= 'a' && latin <= 'z')) {
                             String result = new String(ch);
