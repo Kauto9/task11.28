0908ecc787cc23f00c3f192c5cc658c0f22e5fc9
nikmenshikov <kolya.menshikov.03@list.ru>
2023-01-10 04:16:08 +0300

    version 1.6

--- src/ru/vsu/cs/menshikovnik/Solution.java
+++ src/ru/vsu/cs/menshikovnik/Solution.java
@@ -9,21 +9,23 @@ public class Solution {
         int i = 0;
         List<String> listResult = new ArrayList<>();
         for (String element : str1) {
+            boolean check = false;
             char[] ch = element.toCharArray();
             for (char cyrillic : ch) {
-                if ((cyrillic >= 'А' && cyrillic <= 'Я') || (cyrillic >= 'а' && cyrillic <= 'я')){
                     for (char latin : ch) {
-                        if ((latin >= 'A' && latin <= 'Z') || (latin >= 'a' && latin <= 'z')) {
-                            String result = new String(ch);
-                            listResult.add(i, result);
-                            i++;
-                            break;
+                        if ((cyrillic >= 'А' && cyrillic <= 'Я') || (cyrillic >= 'а' && cyrillic <= 'я')){
+                            if ((latin >= 'A' && latin <= 'Z') || (latin >= 'a' && latin <= 'z')) {
+                                String result = new String(ch);
+                                listResult.add(i, result);
+                                i++;
+                                check = true;
+                                break;
+                            }
                         }
                     }
+                    if (check)break;
                 }
-                break;
             }
-        }
         String delim = " ";
         return String.join(delim, listResult);
     }
